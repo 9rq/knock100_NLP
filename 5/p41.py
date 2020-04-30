1f0b5b4515453d9ef28febaa78a9c15a87c19c2f
l.yoshida.sh <l.yoshida.sh@gmail.com>
2020-04-30 23:03:46 +0900

    first commit

---
+++ 5/p41.py
@@ -0,0 +1,15 @@
+from util import Morph
+from util import Chunk
+
+
+def read_cabocha(file_path = './neko.txt.cabocha'):
+    pass
+
+
+def main():
+    res = read_cabocha()
+    print(res[8])
+
+
+if __name__ == '__main__':
+    main()
